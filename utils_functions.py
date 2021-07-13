65c3e1f69cd0e723fb3019c756c5301daf93773b
SolanaO <silviaEonofrei@gmail.com>
2021-07-13 17:23:21 -0400

    Remove function for merging two dataframes.

--- utils_functions.py
+++ utils_functions.py
@@ -21,27 +21,3 @@ def counts_strings(strings_list, dframe, incol):
     new_df = pd.DataFrame.from_dict(my_counts, orient = 'index').reset_index()
     return new_df
 
-# merge two dataframes and add a clean list of entries 
-def merge_df(df1, df2, old_col):
-    """
-    Merges two dataframes on a common column. 
-    INPUT: 
-        df1, df2 = the two pandas datframes to merge
-        old_col = column shared by the two dataframes
-    OUTPUT:
-        df_temp = new dataframe that contains the common column and all the other 
-                  columns in df1 and df2
-    """
-    
-    df_temp = pd.merge(df1, df2, on = old_col)
-    #df_temp[new_col] = col_list
-    return df_temp
-
-# what other DevTypes do the data coders choose
-data_coders_groups = uf.counts_strings(dev_choice_short,data_coders,'DevType')
-# rename the columns
-data_coders_groups.rename(columns = {'index':'DevTypes', 0:'counts_data'}, inplace=True)
-# add a column with percentages computed with respect to all data coders
-data_coders_groups['perc_data'] = (data_coders_groups.counts_data/data_coders.shape[0] * 100).round(2)
-# show the results ordered by counts and percentages
-data_coders_groups.sort_values('counts_data', ascending=False).head(2)
